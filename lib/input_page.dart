174ab61574e420873ff5108b69b191c0c54ce639
Messi002 <rostandmessi2@gmail.com>
2022-07-11 17:53:09 +0100

    flutterboot

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,14 +10,130 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('BMI Calculator',style: TextStyle(fontFamily:'Pacifico'),),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                    
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                       boxShadow: [
+      BoxShadow(
+        color: Colors.deepPurple.withOpacity(0.5),
+        spreadRadius: 3,
+        blurRadius: 7,
+        offset: Offset(0, 0), // changes position of shadow
       ),
-      body: const Center(
-        child: Text('Body Text',),
+    ],
+                       color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0), 
+                    ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                    
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+      BoxShadow(
+        color: Colors.deepPurple.withOpacity(0.5),
+        spreadRadius: 3,
+        blurRadius: 7,
+        offset: Offset(0, 0), // changes position of shadow
       ),
-      
+    ],
+                       color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0), 
+                      
+                    ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+
+              Expanded(
+                    child: Container(
+                   
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+      BoxShadow(
+        color: Colors.deepPurple.withOpacity(0.5),
+        spreadRadius: 3,
+        blurRadius: 7,
+        offset: Offset(0, 0), // changes position of shadow
+      ),
+    ],
+                       color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0), 
+                    ),
+                    ),
+                  ),
+
+
+
+            Expanded(
+              
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                   
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+      BoxShadow(
+        color: Colors.deepPurple.withOpacity(0.5),
+        spreadRadius: 3,
+        blurRadius: 7,
+        offset: Offset(0, 0), // changes position of shadow
+      ),
+    ],
+                       color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0), 
+                    ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                   
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+      BoxShadow(
+        color: Colors.deepPurple.withOpacity(0.5),
+        spreadRadius: 3,
+        blurRadius: 7,
+        offset: Offset(0, 0), // changes position of shadow
+      ),
+    ],
+                       color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0), 
+                    ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
+        
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
   scaffoldBackgroundColor: Palette.kToDark, 
   appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF0A0E21)),
